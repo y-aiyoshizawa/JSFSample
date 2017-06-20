f2e24587a955aa55a38c2d3f930df9c7ffbcc7e8
TTCAD\y-aiyoshizawa <y-aiyoshizawa@ttc-net.co.jp>
2017-06-20 18:16:53 +0900

    fastcommit

---
+++ src/java/beans/MeiboBean.java
@@ -0,0 +1,42 @@
+package beans;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class MeiboBean {
+    private Integer number;
+    private String name;
+
+    public void toConsole(){
+        System.out.println(this.number + " " + this.name);
+    }
+    /**
+     * @return the number
+     */
+    public Integer getNumber() {
+        return number;
+    }
+
+    /**
+     * @param number the number to set
+     */
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
